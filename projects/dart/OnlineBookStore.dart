ea94cd60063558a8dbda86b99af033f588932615
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-07-02 11:10:27 +0800

    Create OnlineBookStore.dart

---
+++ projects/dart/OnlineBookStore.dart
@@ -0,0 +1,26 @@
+/*
+
+Project Title: Online Bookstore
+
+Description:
+- You have been tasked with developing an online bookstore application. The application should allow users to browse and purchase books from different genres. It should include features like searching for books, adding them to a shopping cart, and checking out to complete the purchase. Additionally, the application should provide functionality for managing user accounts, including registration, login, and password recovery.
+
+Requirements:
+
+1. Implement a class hierarchy for the books, with a base class Book and at least two derived classes representing different genres (e.g., FictionBook, NonFictionBook). Each book should have properties like title, author, price, and availability.
+2. Develop a class ShoppingCart to manage the items selected by the user. It should allow adding and removing books, as well as calculating the total price.
+3. Implement a class User to handle user-related functionality. It should provide methods for registration, login, and password recovery. The user information should be stored securely, considering password encryption and validation.
+4. Create a class OnlineBookstore to serve as the main application. It should include methods for displaying book catalog, searching for books by title or author, and adding/removing books to/from the shopping cart.
+5. Implement error handling throughout the application. Use try-catch blocks to handle exceptions appropriately, such as when a book is out of stock or when an incorrect login is attempted.
+6. Utilize concepts of inheritance, encapsulation, and abstraction in your class design.
+7. Include appropriate getter and setter methods for accessing and modifying object properties.
+8. Provide a user-friendly command-line interface to interact with the application, displaying menus and prompting for user input.
+
+Bonus:
+- Implement a feature for users to write reviews and rate books.
+- Add a recommendation system that suggests books based on the user's browsing history or previous purchases.
+- Store user and book information in a database for persistence.
+
+Remember to structure your code logically, write clear and concise comments, and follow best practices for code organization and style.
+
+*/
